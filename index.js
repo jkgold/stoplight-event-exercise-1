4c58fa7f0df144baef791b3aad970f3036b3d4ab
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-04-24 10:07:32 -0600

    stoplight console.log event listners completed

--- index.js
+++ index.js
@@ -2,4 +2,45 @@
   'use strict';
 
   // YOUR CODE HERE
+  const stopButton = document.querySelector("#stopButton");
+  const slowButton = document.querySelector("#slowButton");
+  const goButton = document.querySelector("#goButton");
+
+  const stopLight = document.querySelector("#stopLight");
+  const slowLight = document.querySelector("#slowLight");
+  const goLight = document.querySelector("#goLight");
+
+  stopButton.addEventListener('click', () => {
+    stopLight.classList.toggle('stop');
+  });
+
+  slowButton.addEventListener('click', () => {
+    slowLight.classList.toggle('slow');
+  });
+
+goButton.addEventListener('click', () => {
+  goLight.classList.toggle('go');
+})
+
+const handleMouseEnter = (event) => {
+  console.log('Entered ${event.target.textContent} button');
+}
+ const handleMouseLeave = (event) => {
+   console.log('Left ${event.target.textContent} button');
+ }
+
+ stopButton.addEventListener('mouseenter', handleMouseEnter);
+
+ slowButton.addEventListener('mouseenter', handleMouseEnter);
+
+ goButton.addEventListener('mouseenter', handleMouseEnter);
+
+ stopButton.addEventListener('mouseleave', handleMouseLeave);
+
+ slowButton.addEventListener('mouseleave', handleMouseLeave);
+
+ goButton.addEventListener('mouseleave', handleMouseLeave);
+
+
+
 })();
